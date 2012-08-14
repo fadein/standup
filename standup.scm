971b3c0d7ef008bfbb40c79cea08adfd57ea697f
Erik Falor <ewfalor@gmail.com>
2012-08-14 16:45:15 -0600

    bell & titlebar support

--- standup.scm
+++ standup.scm
@@ -1,25 +1,38 @@
 #!/var/lib/bin/csi -s
+(use regex-case)
+(use posix)
 
 (define *major* (* 60 20))
 (define *minor* (* 60 2))
 
-(let loop ((timer *major*) (state #t))
-  (sleep 1)
-  (cond
+(define (bell&title str)
+  (regex-case (get-environment-variable "TERM")
+	("screen.*|xterm.*|rxvt.*" _
+	 (print _)  ;DELETE ME
+	 (printf "~a~n]0;~a" str str))
+	(else
+	  (print "TERM=" (get-environment-variable "TERM")))))
 
-	((and (zero? timer) state)
-	 (print "stand up!")
-	 (loop *minor* #f))
+(define (sitdown)
+  (bell&title "sit down!")
+  (let loop ((timer *major*) (state #t))
+	(sleep 1)
+	(cond
 
-	((and (zero? timer) (not state))
-	 (print "sit down!")
-	 (loop *major* #t))
+	  ((and (zero? timer) state)
+	   (bell&title "stand up!")
+	   (loop *minor* #f))
 
-	((zero? (remainder timer 10))
+	  ((and (zero? timer) (not state))
+	   (bell&title "sit down!")
+	   (loop *major* #t))
+
+	  ((zero? (remainder timer 10))
 	   (printf "~a...~n" timer)
 	   (loop (sub1 timer) state))
 
-	(else (loop (sub1 timer) state))))
+	  (else (loop (sub1 timer) state)))))
 
+(sitdown)
 
-
+; vim:set ft=scheme:
