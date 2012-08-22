a5ec156361befecdcd94548d231947bab09b9f90
Erik Falor <ewfalor@gmail.com>
2012-08-22 10:11:03 -0600

    code cleanup & general fixes
    pause between state transitions - force the user to acknowledge them
    flush output at end of bell&title
    comments & variable renames
    stty -echo for clean terminal interaction

--- standup.scm
+++ standup.scm
@@ -3,29 +3,30 @@
 (use srfi-18)
 (use stty)
 
-(define *major* (* 60 20))
-(define *minor* (* 60 2))
+(define *sit-time*   (* 60 20))
+(define *stand-time* (* 60 2))
 (define *interval*   0.125)
 
-(define *stand* "stand up!")
-(define *sit* "sit down")
+(define *stand*      "stand up!")
+(define *sit*        "sit down")
 
 (define (bell&title str)
   (regex-case (get-environment-variable "TERM")
-	("screen.*|xterm.*|rxvt.*" _
-	 ;(print _)  ;DELETE ME
-	 (printf "~a~n]0;~a" str str))
-	(else
-	  (print "TERM=" (get-environment-variable "TERM")))))
+			  ("screen.*|xterm.*|rxvt.*" _
+			   (printf "~a~n]0;~a~!" str str))
+			  (else
+				(print "TERM=" (get-environment-variable "TERM")))))
 
 (define (sitdown)
   (bell&title *sit*)
-  (let loop ((timer *major*) (state #t) (paused #f))
+
+  (let loop ((timer *sit-time*) (state #t) (paused #f))
 
 	(thread-sleep! *interval*)
 
 	(cond
 
+	  ;handle input characters
 	  ((char-ready?)
 	   (let ((char (read-char)))
 		 (if (char=? #\space char)
@@ -38,18 +39,21 @@
 			   (loop (- timer *interval*) state (not paused))))
 		   (loop (- timer *interval*) state paused))))
 
+	  ;timer is paused - NOOP
 	  (paused
 		(loop timer state paused))
 
 	  ;transition from sitting to STANDING
 	  ((and (<= timer 0) state)
 	   (bell&title *stand*)
-	   (loop *minor* #f paused))
+	   (print "Press [space] to continue...")
+	   (loop *stand-time* #f #t))
 
 	  ;transition from STANDING to sitting
 	  ((and (<= timer 0) (not state))
 	   (bell&title *sit*)
-	   (loop *major* #t paused))
+	   (print "Press [space] to continue...")
+	   (loop *sit-time* #t #t))
 
 	  ;print a periodic status msg to indicate where we are
 	  ;TODO: fix this up
@@ -59,6 +63,6 @@
 
 	  (else (loop (- timer *interval*) state paused)))))
 
-(with-stty '(not icanon) sitdown)
+(with-stty '(not icanon echo) sitdown)
 
 ; vim:set ft=scheme:
