aabd8c0d4071b3c329aa5008253aa886b3640b46
Erik Falor <ewfalor@gmail.com>
2012-08-22 10:10:09 -0600

    use finer-grained timer for responsiveness

--- standup.scm
+++ standup.scm
@@ -1,9 +1,11 @@
 #!/var/lib/bin/csi -ns
 (use regex-case)
+(use srfi-18)
 (use stty)
 
 (define *major* (* 60 20))
 (define *minor* (* 60 2))
+(define *interval*   0.125)
 
 (define *stand* "stand up!")
 (define *sit* "sit down")
@@ -19,7 +21,9 @@
 (define (sitdown)
   (bell&title *sit*)
   (let loop ((timer *major*) (state #t) (paused #f))
-	(sleep 1)
+
+	(thread-sleep! *interval*)
+
 	(cond
 
 	  ((char-ready?)
@@ -28,28 +32,32 @@
 		   (cond
 			 (paused
 			   (bell&title (if state *sit* *stand*))
-			   (loop (sub1 timer) state (not paused)))
+			   (loop (- timer *interval*) state (not paused)))
 			 (else
 			   (bell&title "[PAUSED]")
-			   (loop (sub1 timer) state (not paused))))
-		   (loop (sub1 timer) state paused))))
+			   (loop (- timer *interval*) state (not paused))))
+		   (loop (- timer *interval*) state paused))))
 
 	  (paused
 		(loop timer state paused))
 
-	  ((and (zero? timer) state)
+	  ;transition from sitting to STANDING
+	  ((and (<= timer 0) state)
 	   (bell&title *stand*)
 	   (loop *minor* #f paused))
 
-	  ((and (zero? timer) (not state))
+	  ;transition from STANDING to sitting
+	  ((and (<= timer 0) (not state))
 	   (bell&title *sit*)
 	   (loop *major* #t paused))
 
-	  ((zero? (remainder timer 10))
+	  ;print a periodic status msg to indicate where we are
+	  ;TODO: fix this up
+	  ((zero? (remainder (truncate timer) 10))
 	   (printf "~a...~n" timer)
-	   (loop (sub1 timer) state paused))
+	   (loop (- timer *interval*) state paused))
 
-	  (else (loop (sub1 timer) state paused)))))
+	  (else (loop (- timer *interval*) state paused)))))
 
 (with-stty '(not icanon) sitdown)
 
